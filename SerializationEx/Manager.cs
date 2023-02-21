330ab3f84b1730e2dda9c4af5bec173dc40cdf16
LeelasatyasriK <121661802+LeelasatyasriK@users.noreply.github.com>
2023-02-21 17:18:25 +0530

    Add files via upload

---
+++ SerializationEx/Manager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializationEx
+{
+        [Serializable]
+        class Manager : Employee
+        {
+            private double _pa;
+            private double _fa;
+            private double _oa;
+            private double _grossSalary;
+
+            public double PA
+            {
+                get
+                {
+                    PA = 0.08 * Salary;
+                    return _pa;
+                }
+                set
+                {
+                    _pa = value;
+                }
+            }
+            public double FA
+            {
+                get
+                {
+                    FA = 0.13 * Salary;
+                    return _fa;
+                }
+                set { _fa = value; }
+            }
+            public double OA
+            {
+                get
+                {
+                    OA = 0.03 * Salary;
+                    return _oa;
+                }
+                set { _oa = value; }
+            }
+            public double Grosssalary
+            {
+                set { _grossSalary = value; }
+                get
+                {
+                    Grosssalary = PA + FA + OA;
+                    return _grossSalary;
+                }
+            }
+            public void PrintDetails()
+            {
+                Console.WriteLine("Employee Id: " + EmpNo);
+                Console.WriteLine("Employee Name: " + EmpName);
+                Console.WriteLine("Employee Salary: " + Salary);
+            }
+
+            public override void CalculateSalary()
+            {
+                base.CalculateSalary();
+
+            }
+
+        }
+ }
---
+++ SerializationEx/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace SerializationEx
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                MarketingExecutive m = new MarketingExecutive();
+                //Console.WriteLine("Enter Employee Number: ");
+                //m.EmpNo = int.Parse(Console.ReadLine());
+
+                //Console.Write("Enter Employee Name: ");
+                //m.EmpName = Console.ReadLine();
+
+                //Console.Write("Enter Employee salary: ");
+                //m.Salary = int.Parse(Console.ReadLine());
+
+            //SERIALIZATION
+                FileStream stream = new FileStream(@"C:\Users\KOSATYAS\Desktop\MarketingExcecutive.txt", FileMode.OpenOrCreate);
+                BinaryFormatter formatter = new BinaryFormatter();
+            //  formatter.Serialize(stream, m);
+            // stream.Close();
+            //DESERIALIZATION
+             m= (MarketingExecutive)formatter.Deserialize(stream);
+                   Console.WriteLine("Employee id:{0}",m.EmpNo);
+                 Console.WriteLine("Employee Name:{0}", m.EmpName);
+            Console.WriteLine("Employee Name:{0}", m.Salary);
+            stream.Close();
+                 // m.PrintDetails();
+                Console.WriteLine("Enter Kilometers travelled");
+                int n = Convert.ToInt32(Console.ReadLine());
+                m.KT = n;
+                m.TeA = 1000;
+                m.CalculateSalary();
+            }
+        }
+}
+
